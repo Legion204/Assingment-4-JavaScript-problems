2cd34e84a3d5a47af06b27d3f95c2807f77ce1df
Legion204 <ri46801@gmail.com>
2024-02-08 08:49:00 +0600

    added problem 2

--- assinment-4.js
+++ assinment-4.js
@@ -2,22 +2,56 @@
 
 function calculateMoney(ticketSell) {
   const totalTicketPrice = ticketSell * 120;
-  const gurdsalary = 500;
+  const guardSalary = 500;
   const staffLunch = 8 * 50;
-  const totalEarn = totalTicketPrice - (gurdsalary + staffLunch);
-  if(ticketSell<0){
-    const error="invalid Number"
+  const totalEarn = totalTicketPrice - (guardSalary + staffLunch);
+  if (ticketSell < 0) {
+    const error = "invalid Number";
     return error;
-  }
-  else{
+  } else {
     return totalEarn;
   }
 }
 
-
 //problem-2
 
-function checkName(){
+function checkName(name) {
+  if (typeof name !== "string") {
+    const error = "invalid";
+    return error;
+  } else {
+    for (letter of name) {
+      if (
+        name[name.length - 1] === "a" ||
+        name[name.length - 1] === "y" ||
+        name[name.length - 1] === "i" ||
+        name[name.length - 1] === "e" ||
+        name[name.length - 1] === "o" ||
+        name[name.length - 1] === "u" ||
+        name[name.length - 1] === "w" ||
+        name[name.length - 1] === "A" ||
+        name[name.length - 1] === "Y" ||
+        name[name.length - 1] === "I" ||
+        name[name.length - 1] === "E" ||
+        name[name.length - 1] === "O" ||
+        name[name.length - 1] === "U" ||
+        name[name.length - 1] === "W"
+      ) {
+        const good = "Good Name";
+        return good;
+      } else {
+        const bad = "Bad Name";
+        return bad;
+      }
+    }
+  }
+}
+
+//problem-3
+
+function deleteInvalids(){
 
 }
 
+const result = deleteInvalids('badamo');
+console.log(result);
